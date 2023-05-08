c21416d1ec9abec9903c8c6fad9be8fd52408bfa
chandana-gm <chandanagm777@gmail.com>
2023-05-08 12:18:39 +0530

    mechine_test

---
+++ js/file.js
@@ -0,0 +1,99 @@
+function totalAmount() {
+    var name = document.getElementById("name").value
+    var consumerid = document.getElementById("consumerid").value
+    var units = document.getElementById("units").value
+    var Amount = document.getElementById("amount")
+
+    var usedunit = document.getElementById("usedunit")
+    var name2 = document.getElementById("name2")
+    var consumerid2 = document.getElementById("consumerid2")
+
+    var modal = document.getElementById("exampleModal")
+    var modalQuery = document.querySelector('#exampleModal')
+    var login = document.getElementById("login-form")
+    name2.value = name
+    consumerid2.value = consumerid
+    usedunit.value = units
+    if (name != "" && consumerid != "" && units != "") {
+        console.log("inside");
+        // if (consumerid != 5) {
+        //     alert("incorrect consumer id")
+
+        //     return false
+        // }
+        if (units > 0 && units < 100) {
+            var total = units * 5
+            Amount.value = total
+
+        }
+        else if (units >= 100 && units < 150) {
+            var total1 = units * 5
+            console.log(total1);
+            var total2 = units - 100
+            console.log(total2);
+            var total3 = total2 * 5
+            console.log(total3);
+
+            var total4 = total1 + total3
+            Amount.value = total4
+
+        }
+        else if (units >= 150 && units < 200) {
+            var total5 = units * 5
+            var total6 = units - 100
+            var total7 = total6 * 8
+            var total8 = total5 + total7
+            Amount.value = total8
+
+        }
+
+
+        else if (units >= 200) {
+            var total5 = units * 5
+            var total6 = units - 100
+            var total7 = total6 * 10
+            var total8 = total5 + total7
+            Amount.value = total8
+
+        }
+        else {
+            console.log("THE UNIT VALUE CANNOT BE ZERO");
+            alert("THE UNIT VALUE CANNOT BE ZERO")
+            // modal.style.display = "none";
+            var body = document.querySelector("body")
+            var modalBackDrop = document.querySelector(".modal-backdrop")
+            modalBackDrop.classList.remove('modal-backdrop')
+            body.classList.remove('modal-open')
+            // modal.classList.add("d-none");        
+            modalQuery.parentNode.removeChild(modalQuery);
+            // body.classList.remove('modal-show')
+            // modal.classList.add("d-none");
+            // login.style.display = "block";
+            // return false
+        }
+
+
+
+    }
+    else {
+        alert("enter the values properly")
+        var body = document.querySelector("body")
+        var modalBackDrop = document.querySelector(".modal-backdrop")
+        modalBackDrop.classList.remove('modal-backdrop')
+        body.classList.remove('modal-open')
+        // modal.classList.add("d-none");        
+        modalQuery.parentNode.removeChild(modalQuery);
+
+
+    }
+}
+
+// function close(){
+//     var name = document.getElementById("name")
+//     var consumerid = document.getElementById("consumerid")
+//     var units = document.getElementById("units")
+
+//     name.value = ""
+//     consumerid.value = ""
+//     units.value =""
+// }
